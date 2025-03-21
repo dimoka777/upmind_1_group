c4021b953ecbe084d7339df13156c5567b1c1ef1
dimoka777 <dimokavip777@gmail.com>
2025-03-21 14:35:00 +0800

    first commit

---
+++ lesson1.py
@@ -0,0 +1,114 @@
+
+
+# Однострочные комментарии
+
+"""
+Многосрочные
+комментарии
+1
+2
+3
+"""
+
+# int - (числа/integer) не изменяемый целочисленный тип данных
+num1 = 1
+num2 = 2
+num3 = 3
+
+# age = int(input("Введите ваш возраст: "))  # Ввод числа
+# print("Ваш возраст: ", age)
+# print(type(age))  # <class 'int'>
+#
+a = 10
+b = 32
+print(a + b + 2)  # 13 (сложение)
+print(a - b / 22)  # 7 (вычитание)
+print(a * b)  # 30 (умножение)
+print(a // b)  # 3 (целочисленное деление)
+print(a % b)  # 1 (остаток от деления)
+print(a ** b)  # 1000 (возведение в степень)
+
+# pi = float(input("Введите число с плавающей точкой: "))  # Ввод float
+# print("Вы ввели:", pi)
+# print(type(pi))  # <class 'float'>
+# print(round(pi, 2))  # Округление
+#
+# num_str = "3.5"
+# num_float = float(num_str)
+# print(type(num_float))  # <class 'float'>
+
+
+# str - (строка/string) не изменяемый строковый тип данных
+s1 = "string"
+enter = 'Логин'
+
+# name = input("Введите ваше имя: ")  # Ввод строки
+# print("Привет, " + name + "!")
+# print(type(name))  # <class 'str'>
+# text = """Это многострочный текст в Python"""
+# print(text)
+# word = "Python"
+# print(word)
+# print(word[0])  # P
+# print(word[-1])  # n
+# print(len(word))  # 6
+# number = 42
+# num_str = str(number)
+# print(type(num_str))  # <class 'str'>
+
+a = 3
+b = 10
+# bool - может иметь только два состояния: True or False
+# is_python_easy = input("Python легкий? (да/нет): ") == "да"
+# print("Вы ответили:", is_python_easy)
+# print(type(is_python_easy))  # <class 'bool'>
+# a = b
+# print(a >= b)  # False
+# print(a <= b)  # True
+# print(a == b)  # False
+# print(a != b)  # True
+# print(True and False)  # False
+# print(True or False)  # True
+# print(not True)  # False
+
+
+
+
+# Преобразование типов
+print(float(5))  # 5.0
+print(int(5.9))  # 5
+print(int("100"))  # 100
+print(str(42))  # "42"
+print(int(True))  # 1
+print(int(False))  # 0
+
+
+
+# print()  - вывод в консоль, принимает на вход переменные можно через запятую
+# input()  - ввод с консоли, принимает на вход текст который выведет в консоль для ввода
+# type() - выводит тип данных переменной, принимает на вход переменную
+# len() - выводит длину объекта, принимает на вход переменную
+# int() - приведение к типу integer
+# str() - приведение к типу string
+# float() - приведение к типу float
+# lower() - метод строки переводящий всю строку в нижний регистр
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
